b24757a830c09279c555995ddc90db889a5683bb
zachsirera <z.sirera@gmail.com>
2019-02-27 22:11:35 -0600

    added placeholders for future functions. removed ugly list by referencing existing list in helpers file

--- ai1.py
+++ ai1.py
@@ -20,20 +20,7 @@ import helpers
 ## Play Rules ##
 
 
-cards_list = [
-	{"face": "2", "value": 2, "score_value": 2, "deal_2": 5, "deal_3": 5, "deal_4": 5},  
-	{"face": "3", "value": 3, "score_value": 3, "deal_2": 5, "deal_3": 5, "deal_4": 5},  
-	{"face": "4", "value": 4, "score_value": 4, "deal_2": 5, "deal_3": 5, "deal_4": 5},  
-	{"face": "5", "value": 5, "score_value": 5, "deal_2": 5, "deal_3": 5, "deal_4": 5},  
-	{"face": "6", "value": 6, "score_value": 6, "deal_2": 6, "deal_3": 6, "deal_4": 6},  
-	{"face": "7", "value": 7, "score_value": 7, "deal_2": 7, "deal_3": 7, "deal_4": 7},  
-	{"face": "8", "value": 8, "score_value": 8, "deal_2": 8, "deal_3": 8, "deal_4": 7},  
-	{"face": "9", "value": 9, "score_value": 9, "deal_2": 9, "deal_3": 9, "deal_4": 7},  
-	{"face": "10", "value": 10, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
-	{"face": "J", "value": 11, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7}, 
-	{"face": "Q", "value": 12, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7}, 
-	{"face": "K", "value": 13, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
-	{"face": "A", "value": 14, "score_value": 11, "deal_2": 11, "deal_3": 11, "deal_4": 7}]
+
 
 # This is a variable to test the functions. To be removed.
 test_hand = [{"face": "2", "value": 2, "score_value": 2, "deal_2": 5, "deal_3": 5, "deal_4": 5},  
@@ -41,8 +28,9 @@ test_hand = [{"face": "2", "value": 2, "score_value": 2, "deal_2": 5, "deal_3": 
 	{"face": "4", "value": 4, "score_value": 4, "deal_2": 5, "deal_3": 5, "deal_4": 5},  
 	{"face": "5", "value": 5, "score_value": 5, "deal_2": 5, "deal_3": 5, "deal_4": 5}, 
 	{"face": "9", "value": 9, "score_value": 9, "deal_2": 9, "deal_3": 9, "deal_4": 7},
-	{"face": "J", "value": 11, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
-	{"face": "J", "value": 11, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
+	{"face": "10", "value": 10, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
+	{"face": "10", "value": 10, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
+	{"face": "10", "value": 10, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7},
 	{"face": "J", "value": 11, "score_value": 10, "deal_2": 10, "deal_3": 10, "deal_4": 7}]
 
 
@@ -87,13 +75,23 @@ def toss(hand):
 	# Create and return hand based on remaining items in face_list
 	new_hand = []
 	for value in face_list:
-		card = next((x for x in cards_list if x['face'] == value), None)
+		card = next((x for x in helpers.cards_list if x['face'] == value), None)
 		new_hand.append(card)
-
-	helpers.display_hand(new_hand, 1)
 
 	return new_hand
 
+
+
+
+def lead(hand):
+	''' This is a function to apply the rules listed above for the lead '''
+	pass
+
+
+
+def play(hand, current_play):
+	''' This is a function to apply the rules listed above for playing in response to the current play '''
+	pass
 
 
 
